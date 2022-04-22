b0a1b5ac51dadfbdd2c121e96d7c105b08589465
Macgameur <maceo.jalbert@gmail.com>
2022-04-22 11:39:18 +0200

    :feat: LOVE

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -1,29 +1,48 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MoveTo : MonoBehaviour
 {
+    [SerializeField]
     private GameObject[] animals;
-    private int animalIndex;
+    private int animalIndex = 0;
     private NavMeshAgent agent;
+    private SpawnAnimals animalSpawner;
+    [SerializeField]
+    private float gestationTime = 3;
 
     void Start()
     {
         animals = GameObject.FindGameObjectsWithTag("Animal");
+        animalSpawner = GameObject.FindGameObjectWithTag("AnimalSpawner").GetComponent<SpawnAnimals>();
         agent = GetComponent<NavMeshAgent>();
-        
-        agent.destination = animals[animalIndex].transform.position;
     }
 
     private void Update()
     {
-        agent.destination = animals[animalIndex].transform.position;
-        if (agent.enabled && agent.remainingDistance < 0.5f)
+        if (animals[animalIndex].transform.position == gameObject.transform.position)
         {
+            animalIndex = animals.Length > animalIndex + 1 ? animalIndex + 1 : 0;
+        }
+        agent.destination = animals[animalIndex].transform.position;
+        if (agent.enabled && agent.remainingDistance < 1 && agent.remainingDistance > 0.5f)
+        {
+            StartCoroutine(Love());
             if (animalIndex < animals.Length - 1)
             {
-                animalIndex++;
+                animalIndex += 1;
+            } else
+            {
+                animals = GameObject.FindGameObjectsWithTag("Animal");
+                animalIndex = 0;
             }
         }
+    }
+
+    IEnumerator Love()
+    {
+        yield return new WaitForSeconds(gestationTime);
+        animalSpawner.SpawnAnimalAtPos(gameObject, transform.position, transform.rotation);
     }
 }
--- Assets/Scripts/SpawnAnimals.cs
+++ Assets/Scripts/SpawnAnimals.cs
@@ -7,40 +7,58 @@ public class SpawnAnimals : MonoBehaviour
     [SerializeField]
     private GameObject[] animals;
     [SerializeField][Min(2)]
-    private int maxAnimalsNbr = 50;
+    private int maxAnimalsNbr = 10000;
+    [SerializeField]
+    private int animalsSpawnNbr = 50;
+    [SerializeField]
+    private bool randomNbr = true;
 
     [SerializeField]
     private float maxX = 100;
     [SerializeField]
     private float maxZ = 100;
 
+    private float animalsNbr = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < animals.Length; i++)
         {
-            SpawnXAnimals(animals[i], Random.Range(2, maxAnimalsNbr));
+            SpawnXAnimals(animals[i], randomNbr ? Random.Range(2, animalsSpawnNbr) : animalsSpawnNbr);
         }
     }
 
-    public void SpawnXAnimals(GameObject animal,  int nbrOfAnimals)
+    public void SpawnXAnimals(GameObject animal, int nbrOfAnimals)
     {
         Vector3 position = Vector3.zero;
         Quaternion rotation = Quaternion.identity;
 
         for (int i = 0; i < nbrOfAnimals; i++)
         {
-            float random_nbr = Random.Range(0, 1000);
-            position.x = random_nbr % maxX;
-            position.z = (random_nbr + random_nbr) % maxZ;
-            rotation.y = (random_nbr + random_nbr) % 360;
+            position.x = Random.Range(0, maxX);
+            position.y = 1000;
+            position.z = Random.Range(0, maxZ);
+            rotation.y = Random.Range(0, 360);
             RaycastHit hit;
-            if (Physics.Raycast(position, Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("terrain"), QueryTriggerInteraction.UseGlobal))
+            if (Physics.Raycast(position, Vector3.down, out hit, Mathf.Infinity) && animalsNbr < maxAnimalsNbr)
             {
-                Debug.Log("Spawn");
                 position.y -= hit.distance + 2;
                 Instantiate(animal, position, rotation);
+                animalsNbr++;
             }
+        }
+    }
+
+    public void SpawnAnimalAtPos(GameObject animal, Vector3 position, Quaternion rotation)
+    {
+        RaycastHit hit;
+        position.y += 100;
+        if (Physics.Raycast(position, Vector3.down, out hit, Mathf.Infinity) && animalsNbr < maxAnimalsNbr)
+        {
+            position.y -= hit.distance + 2;
+            Instantiate(animal, position, rotation);
+            animalsNbr++;
         }
     }
 }
